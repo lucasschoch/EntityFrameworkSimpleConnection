ea40acc57ecb4c504a024b151c0058239453bfdb
Lucas Schoch <lucas.schoch@atitude.com.br>
2018-03-26 15:51:01 -0300

    First Commit

---
+++ ExemploConexaoEntity/ExemploModel.edmx.sql
@@ -0,0 +1,7 @@
+
+
+set identity_insert PessoasSet OFF
+insert into PessoasSet (Id) values (1);
+insert into PessoasSet (Id) values (2);
+insert into PessoasSet (Id) values (3);
+insert into PessoasSet (Id) values (4);
---
+++ ExemploConexaoEntity/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploConexaoEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ExemploModelContainer ctx = new ExemploModelContainer())
+            {
+                var pessoas = from p in ctx.PessoasSet
+                              select p.Id;
+                var listPessoas = pessoas.ToList();
+
+                foreach (int id in listPessoas)
+                {
+                    Console.WriteLine(id);
+                }
+            }
+        }
+    }
+}
